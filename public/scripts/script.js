fcb76a735ffca55d448870f40676e766bea27e0c
Dana Centeno <danacentenodes@gmail.com>
2016-05-26 18:44:51 -0400

    figuring out loading page on JS

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,7 +13,7 @@ app.init = function () {
 app.displayCard = function (x, data) {
 	var cardImg = $('<figure>').css('background-image', 'url(' + data.results[x].Images[0].url_570xN + ')');
 	var cardUrl = data.results[x].url;
-	var cardTitle = $('<h3>').html("<a href=" + cardUrl + ">" + data.results[x].title + "</a>");
+	var cardTitle = $('<figcaption>').html('<a href=' + cardUrl + '> <div class="wrapper"> <h3>' + data.results[x].Shop.shop_name + '</h3>' + '<p>' + data.results[x].title + '</p> </div> </a>');
 
 	//Concatenate all the HTML elements
 	var cardDiv = $('<div>').addClass('card card' + x).append(cardImg, cardTitle);
@@ -30,12 +30,8 @@ app.getData = function (userLocation) {
 			api_key: app.apikey,
 			keywords: "ceramic",
 			location: userLocation,
-<<<<<<< HEAD
-			includes: "Images:1, Shop(shop_name)"
-=======
-			includes: 'Images:1',
+			includes: "Images:1, Shop(shop_name)",
 			limit: 100
->>>>>>> 4ff007f541900792f0b9ece410f4bc877c9e5d13
 		},
 		success: function success(data) {
 			app.data = data;
@@ -43,25 +39,10 @@ app.getData = function (userLocation) {
 			$('.cards').empty();
 			console.log("Location entered: ", userLocation);
 			console.log("Data from Etsy: ", data);
-<<<<<<< HEAD
 
 			//Display the first 25 results
 			for (var x = 0; x < 25; x++) {
 				app.displayCard(x, data);
-=======
-			var x = 0;
-			for (x in data.results) {
-
-				var cardImg = $('<figure>').css('background-image', 'url(' + data.results[x].Images[0].url_570xN + ')');
-				var cardUrl = data.results[x].url;
-				var cardTitle = $('<figcaption>').html('<a href=' + cardUrl + '> <div class="wrapper"> <h3>' + data.results[x].Shop.shop_name + '</h3>' + '<p>' + data.results[x].title + '</p> </div> </a>');
-
-				//Concatenate all the HTML elements
-				var cardDiv = $('<article>').addClass('card card' + x).append(cardImg, cardTitle);
-
-				//Post them on the page
-				$('.cards').append(cardDiv);
->>>>>>> ebca4520001436648befa9dadd3ab95424d888b8
 			}
 			app.mostRecentlyCalledElement = 25;
 		},
