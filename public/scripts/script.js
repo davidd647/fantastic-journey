b32150c8bd2b75207480ae664555d38300e60510
CJ Reyes <itscjreyes@gmail.com>
2016-05-26 14:42:11 -0400

    Updated header css/html

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -24,9 +24,6 @@ app.getData = function () {
 			console.log(data, 'success');
 			var x = 0;
 			for (x in data.results) {
-<<<<<<< HEAD
-				$('div.cards').append('<img src="' + data.results[x].Images[0].url_570xN + '" alt="' + data.results[x].title + '">').append('<h3>' + data.results[x].title + '</h3>').append('<p class="description">' + data.results[x].description + ' </p>');
-=======
 
 				var cardImg = $('<img>').attr('src', data.results[x].Images[0].url_570xN);
 				var cardTitle = $('<h3>').text(data.results[x].title);
@@ -37,7 +34,6 @@ app.getData = function () {
 
 				//Post them on the page
 				$('.cards').append(cardDiv);
->>>>>>> c02973e2d2f47138550ac2dd6ad305a36c031f1e
 			}
 		},
 		error: function error(data) {
