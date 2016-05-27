42f25749ee354d83cc52dffb191906c47c548bca
Dana Centeno <danacentenodes@gmail.com>
2016-05-27 14:40:19 -0400

    adding smoothscroll

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -115,22 +115,14 @@ $('.more_cards').on('click',function(){
 });
 
 $(function(){
+	$('.search').on('click', function(){
+		
+	});
+
+
 	app.init();
 
-	// function smoothScroll(){
-	// 	$('a[href^="#"]').on('click',function (e) {
-	//     e.preventDefault();
-
-	//     var target = this.hash;
-	//     var $target = $(target);
-
-	//     $('html, body').stop().animate({
-	//         'scrollTop': $target.offset().top
-	//     }, 1000, 'swing', function () {
-	//         window.location.hash = target;
-	//     });
-	// 	});
-	// };
+	
 });
 
 
