fb8ba2aac070df579bc64c9dc55a5ce089decf9b
Dana Centeno <danacentenodes@gmail.com>
2016-05-27 10:38:35 -0400

    played around with the JS and HTML for loading page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,18 +8,12 @@ app.userLocation = "Toronto";
 
 app.init = function () {
 	// app.getData(); //we want to wait for user input first
-};
 
-app.displayCard = function (x, data) {
-	var cardImg = $('<figure>').css('background-image', 'url(' + data.results[x].Images[0].url_570xN + ')');
-	var cardUrl = data.results[x].url;
-	var cardTitle = $('<figcaption>').html('<a href=' + cardUrl + '> <div class="wrapper"> <h3>' + data.results[x].Shop.shop_name + '</h3>' + '<p>' + data.results[x].title + '</p> </div> </a>');
+	// When user submits their location, a loading page will show up briefly until the results are revealed!
 
-	//Concatenate all the HTML elements
-	var cardDiv = $('<div>').addClass('card card' + x).append(cardImg, cardTitle);
-
-	//Post them on the page
-	$('.cards').append(cardDiv);
+	$('.search').on('click', function () {
+		$('.loading').addClass('showMe');
+	});
 };
 
 app.getData = function (userLocation) {
@@ -30,21 +24,25 @@ app.getData = function (userLocation) {
 			api_key: app.apikey,
 			keywords: "ceramic",
 			location: userLocation,
-			includes: "Images:1, Shop(shop_name)",
-			limit: 100
+			includes: 'Images:1'
 		},
 		success: function success(data) {
-			app.data = data;
-			console.log("This global variable is assigned as: ", app.data);
 			$('.cards').empty();
 			console.log("Location entered: ", userLocation);
 			console.log("Data from Etsy: ", data);
+			var x = 0;
+			for (x in data.results) {
 
-			//Display the first 25 results
-			for (var x = 0; x < 25; x++) {
-				app.displayCard(x, data);
+				var cardImg = $('<figure>').css('background-image', 'url(' + data.results[x].Images[0].url_570xN + ')');
+				var cardUrl = data.results[x].url;
+				var cardTitle = $('<figcaption>').html("<a href=" + cardUrl + ">" + data.results[x].title + "</a>");
+
+				//Concatenate all the HTML elements
+				var cardDiv = $('<article>').addClass('card card' + x).append(cardImg, cardTitle);
+
+				//Post them on the page
+				$('.cards').append(cardDiv);
 			}
-			app.mostRecentlyCalledElement = 25;
 		},
 		error: function error(data) {
 			console.log(data, 'error');
@@ -57,21 +55,6 @@ $('.search').on('click', function (e) {
 
 	var locationInput = $('input').val();
 	app.getData(locationInput);
-});
-
-$('.more_cards').on('click', function () {
-	//Display next 10 results
-	if (app.mostRecentlyCalledElement < 100) {
-		for (var x = app.mostRecentlyCalledElement; x < app.mostRecentlyCalledElement + 10; x++) {
-			app.displayCard(x, app.data);
-			if (x >= 99) {
-				app.mostRecentlyCalledElement = 100;
-			}
-		}
-		app.mostRecentlyCalledElement += 10;
-	} else {
-		$('more_cards').append("There are no more items...");
-	}
 });
 
 $(function () {
